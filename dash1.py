15179f1cb97b8225f9a98e5ef08d42505b2f6525
Gabriel <gabriellima878@gmail.com>
2024-11-17 17:35:46 -0300

    novos recursos adicionados, como filtros, botões dinâmicos etc

--- dash1.py
+++ dash1.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -9,8 +9,16 @@ df = pd.read_excel('Vendas.xlsx')
 
 # plotly
 fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+fig2 = px.scatter(df, x="Quantidade", y="Valor Final", color="Produto", size="Valor Unitário", size_max=60)
 
-#css 
+# lista_marcas = ["Treinamentos", "Programação", "Todas"]
+lista_marcas = list(df["Marca"].unique())
+lista_marcas.append("Todas")
+
+lista_paises = list(df["País"].unique())
+lista_paises.append("Todos")
+
+# Layout 
 app.layout = html.Div(children=[
     html.H1(children='Meu Dashboard'),
 
@@ -18,16 +26,57 @@ app.layout = html.Div(children=[
         Dashboard de Vendas em Python
     '''),
 
-    html.H3(children="Vendas de cada Produto por Loja"),
+    html.H3(children="Vendas de cada Produto por Loja", id="subtitulo"),
+
+    dcc.RadioItems(lista_marcas, value="Todas", id="selecao_marcas", inline=True),
+
+    html.Div(children=[
+        dcc.Dropdown(lista_paises, value="Todos", id="selecao_pais"),   
+    ], style={"width": "50%", "margin": "auto"}),
 
     dcc.Graph(
-        id='example-graph',
+        id='vendas_por_loja',
         figure=fig
-    )
+    ),
+
+    dcc.Graph(
+        id='distribuicao_vendas',
+        figure=fig2
+    ),
+
+
 ], style={"text-align": "center"})
 
 # callbacks
 
+@app.callback(
+    Output("subtitulo", "children"),
+    Output("vendas_por_loja", "figure"),
+    Output("distribuicao_vendas", "figure"),
+    Input("selecao_marcas", "value"),
+    Input("selecao_pais", "value"),
+)
+def selecionar_marca(marca, pais):
+    if marca == "Todas" and pais == "Todos":
+        texto = "Vendas de cada Produto por Loja"
+        fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+        fig2 = px.scatter(df, x="Quantidade", y="Valor Final", color="Produto", size="Valor Unitário", size_max=60)
+    else:
+        df_filtrada = df
+        if marca != "Todas":
+            df_filtrada = df_filtrada.loc[df_filtrada['Marca'] == marca, : ]
+            # filtrar de acordo com a marca
+        if pais != "Todos":
+            df_filtrada = df_filtrada.loc[df_filtrada["País"] == pais, : ]
+            # filtrar de acordo com o país
+
+
+        texto = f"Vendas de cada Produto por Loja da Marca {marca} e do País {pais}"
+        fig = px.bar(df_filtrada, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+        fig2 = px.scatter(df_filtrada, x="Quantidade", y="Valor Final", color="Produto", size="Valor Unitário", size_max=60)
+    return texto, fig, fig2
+
+
 # colocando o site no ar
 if __name__ == '__main__':
-    app.run(debug=True, port=5050)
+    app.run(debug=True, port=8050)
